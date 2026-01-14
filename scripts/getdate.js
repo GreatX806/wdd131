4dcdf5b6593762e3748c2648764516c6330d3ee0
GreatX806 <your-email@example.com>
2026-01-14 15:14:44 +0100

    week1 assignment completed

--- scripts/getdate.js
+++ scripts/getdate.js
@@ -1,4 +1,19 @@
-document.getElementById("year-now").textContent = new Date().getFullYear();
+'use strict';
 
-document.getElementById("page-update").textContent =
-  "Last updated: " + document.lastModified;
+document.addEventListener('DOMContentLoaded', () => {
+  try {
+    const currentYearSpan = document.getElementById('currentyear');
+    const currentYear = new Date().getFullYear();
+    currentYearSpan.textContent = currentYear;
+  } catch (e) {
+    console.error('Error setting current year:', e);
+  }
+
+  try {
+    const lastModifiedParagraph = document.getElementById('lastModified');
+    const lastModifiedDate = document.lastModified;
+    lastModifiedParagraph.textContent = `Last Modification: ${lastModifiedDate}`;
+  } catch (e) {
+    console.error('Error setting last modified date:', e);
+  }
+});
